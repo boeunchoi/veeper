c79027515cdc9ba7e39d95330cb0c0eb2b9ac4d9
mattcwilde <mattcwilde@gmail.com>
2020-11-11 18:09:47 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from joebvp import atomicdata
 from setuptools import setup, find_packages
 
 setup(name='joebvp',
-      version='0.1',
+      version='0.2',
       description=
       'Software for efficiently fitting Voigt profiles to UV spectra.',
       url='http://github.com/jnburchett/joebvp',
