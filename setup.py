cdf383536c06157b58cf388b1d5e2dabe6a24238
mattcwilde <mattcwilde@gmail.com>
2020-11-11 19:25:56 -0700

    get atomic data with pip

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from numpy.core.multiarray import packbits
+#from numpy.core.multiarray import packbits
 from joebvp import atomicdata
 from setuptools import setup, find_packages
+import glob
+from pkg_resources import resource_filename
+
+data_files = []
+root_path = resource_filename('joebvp', 'atomicdata/')
+directories = glob.glob(root_path)
+for directory in directories:
+    files = glob.glob(directory + '*')
+    data_files.append((directory, files))
+# then pass data_files to setup()
 
 setup(name='joebvp',
-      version='0.3',
+      version='0.4',
       description=
       'Software for efficiently fitting Voigt profiles to UV spectra.',
       url='http://github.com/jnburchett/joebvp',
       author='Joseph N. Burchett',
       author_email='jburchet@astro.umass.edu',
+      data_files=data_files,
       packages=find_packages())
 # packages=['joebvp', 'joebvp.atomicdata', 'joebvp.LSF'])
